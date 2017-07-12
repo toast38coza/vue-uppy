3cb0d3ab838eee1d46f8f2776452bc69b5b1aa14
toast38coza <info@38.co.za>
2017-07-12 08:39:02 +0200

    Rename component to uppy

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <uppy></uppy>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Uppy from './components/Uppy'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Uppy
   }
 }
 </script>
